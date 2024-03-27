f13f0eccbb613a7d990b02068d9bcb573b591805
yolandaproensa <yolandaproensa@gmail.com>
2024-03-27 20:02:06 -0300

    Pull request

--- recebeDados.test.js
+++ recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Yolanda Proensa" e o RA "191648"', () => {
+    const nome = "Yolanda Proensa";
+    const RA = "191648";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
